bb4107f6a12420ac95ff638f9c2f1d3b0885ff39
Valentina E <tiyahelfrih@gmail.com>
2020-08-25 12:18:04 +0000

    Automatically backed up by Learn

--- lib/min.rb
+++ lib/min.rb
@@ -1,3 +1,10 @@
 def get_the_min(groceries)
-  groceries.values.flatten.min
+groceries = {
+  dairy: ["milk", "yogurt", "cheese"],
+  vegetable: ["carrots", "broccoli", "cucumbers"],
+  meat: ["chicken", "steak", "salmon"],
+  grains: ["rice", "pasta"]
+}
+
+groceries.values.flatten.min
 end
